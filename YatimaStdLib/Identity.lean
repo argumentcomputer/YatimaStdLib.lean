21ab2554aee6741b8e466dc1578930e895412ed6
Hanting Zhang <hantingz@usc.edu>
2022-07-06 15:57:47 -0700

    remove `id A -> A` coe

--- YatimaStdLib/Identity.lean
+++ YatimaStdLib/Identity.lean
@@ -3,10 +3,7 @@ namespace Identity
 structure Identity (A : Type α) where
   runIdentity : A
 
-@[inline] instance idToA : Coe (Identity A) A where
-  coe a := a.runIdentity
-
-@[inline] instance aToIdA : Coe A (Identity A) where
+instance : Coe A (Identity A) where
   coe a := Identity.mk a
 
 end Identity
