868abcb58c5d96927c65ac304ee1e99a21d57457
mpenciak <matej.penciak@gmail.com>
2022-10-03 15:06:44 -0400

    Add notation precedence for Zmod notation

--- YatimaStdLib/Zmod.lean
+++ YatimaStdLib/Zmod.lean
@@ -13,12 +13,12 @@ def rep (a : Zmod n) : Int := a
 instance : Add (Zmod n) where
   add (a b : Zmod n) := (rep a + rep b) % (n : Int)
 
-notation a"  + " b " mod " n => Zmod.add (a : Zmod n) (b : Zmod n)
+notation:50 a "  + " b " mod " n => Add.add (a : Zmod n) (b : Zmod n)
 
 instance : Mul (Zmod n) where
   mul (a b : Zmod n) := (rep a * rep b) % (n : Int)
 
-notation a " * " b " mod " n => Zmod.mul (a : Zmod n) (b : Zmod n)
+notation:51 a " * " b " mod " n => Mul.mul (a : Zmod n) (b : Zmod n)
 
 instance : Inhabited (Zmod n) where
   default := 0
@@ -37,7 +37,7 @@ instance : Pow (Zmod n) Nat where
 instance : Sub (Zmod n) where
   sub (a b : Zmod n) := (rep a - rep b) % (n : Int)
 
-notation a " - " b " mod " n => Zmod.sub (a : Zmod n) (b : Zmod n)
+notation:50 a " - " b " mod " n => Sub.sub (a : Zmod n) (b : Zmod n)
 
 def modInv (a : Zmod n) : Zmod n := Int.modInv a n
 
