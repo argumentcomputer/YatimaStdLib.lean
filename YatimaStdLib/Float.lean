02393c61a6ecf97789dc7e15e975ff97c72fb431
mpenciak <matej.penciak@gmail.com>
2022-10-19 22:40:29 -0400

    fix Float namespace

--- YatimaStdLib/Float.lean
+++ YatimaStdLib/Float.lean
@@ -2,7 +2,7 @@ namespace Float
 
 open Float (log)
 
-def Float.toNat : Float → Nat := USize.toNat ∘ Float.toUSize
+def toNat : Float → Nat := USize.toNat ∘ Float.toUSize
 
 def logBase (base arg : Float) : Float := log arg / log base
 
