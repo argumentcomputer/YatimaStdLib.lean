03d869029df1306ecdb0f452fc796d9bcf785ed1
DanielRrr <daniel.rogozin@serokell.io>
2022-11-18 18:25:33 +0000

    define the monad instance for array

--- YatimaStdLib/Array.lean
+++ YatimaStdLib/Array.lean
@@ -3,3 +3,11 @@ def Array.iota (n : Nat) : Array Nat :=
   match n with
     | 0 => #[0]
     | k+1 => Array.push (iota k) (k + 1)
+
+def Array.join (l : Array (Array A)) : Array A :=
+  Array.foldr (. ++ .) #[] l
+
+instance : Monad Array where
+  map := Array.map
+  pure x := #[x]
+  bind l f := Array.join $ Array.map f l
