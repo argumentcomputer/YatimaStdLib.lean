d6add8a9b6ebb702648f913a5f1143b9b3de6338
Daniel Rogozin <relationalgebra@gmail.com>
2022-11-18 18:57:15 +0000

    Accept Arthur's suggestion
    Co-authored-by: Arthur Paulino <arthurleonardo.ap@gmail.com>

--- YatimaStdLib/Array.lean
+++ YatimaStdLib/Array.lean
@@ -1,8 +1,6 @@
 -- generates the array of nats from 0,...,n by a given n
 def Array.iota (n : Nat) : Array Nat :=
-  match n with
-    | 0 => #[0]
-    | k+1 => Array.push (iota k) (k + 1)
+  Array.mk (List.range n) |>.push n
 
 def Array.join (l : Array (Array A)) : Array A :=
   Array.foldr (. ++ .) #[] l
