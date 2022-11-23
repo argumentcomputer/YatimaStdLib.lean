020db9d16a651c75effcd6615dfcee39b24fbc38
DanielRrr <daniel.rogozin@serokell.io>
2022-11-23 13:51:48 +0000

    fixing the name (2)

--- Tests/USize.lean
+++ Tests/USize.lean
@@ -3,8 +3,8 @@ import YatimaStdLib.USize
 
 open Lean LSpec USize
 
-#lspec test "next_power_of_two works" (next_power_of_two 5 == 8) $
-       test "next_power_of_two works" (next_power_of_two 100 == 128) $
-       test "next_power_of_two works" (next_power_of_two 1000 == 1024) $
-       test "next_power_of_two works" (next_power_of_two 0 == 1) $
-       test "next_power_of_two works" (next_power_of_two 30000 = 32768)
+#lspec test "next_power_of_two works" (nextPowerOfTwo 5 == 8) $
+       test "next_power_of_two works" (nextPowerOfTwo 100 == 128) $
+       test "next_power_of_two works" (nextPowerOfTwo 1000 == 1024) $
+       test "next_power_of_two works" (nextPowerOfTwo 0 == 1) $
+       test "next_power_of_two works" (nextPowerOfTwo 30000 = 32768)
