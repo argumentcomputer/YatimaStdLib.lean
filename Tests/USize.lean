ea96c587a3f9e8f65719b6f4f70f1305c560c6ac
Daniel Rogozin <relationalgebra@gmail.com>
2022-11-23 13:59:51 +0000

    Define the function that computes the next power of two (#43)
    * define next power of two for nova
    
    * fixing the name
    
    * fixing the name (2)

---
+++ Tests/USize.lean
@@ -0,0 +1,10 @@
+import LSpec
+import YatimaStdLib.USize
+
+open Lean LSpec USize
+
+#lspec test "next_power_of_two works" (nextPowerOfTwo 5 == 8) $
+       test "next_power_of_two works" (nextPowerOfTwo 100 == 128) $
+       test "next_power_of_two works" (nextPowerOfTwo 1000 == 1024) $
+       test "next_power_of_two works" (nextPowerOfTwo 0 == 1) $
+       test "next_power_of_two works" (nextPowerOfTwo 30000 = 32768)
