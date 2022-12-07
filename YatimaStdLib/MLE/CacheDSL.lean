43d63ea60c0dabe0945c8909c0e9a05dccc46ed6
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-12-07 17:00:18 -0300

    small optimizations in the cache DSL

--- YatimaStdLib/MLE/CacheDSL.lean
+++ YatimaStdLib/MLE/CacheDSL.lean
@@ -18,24 +18,25 @@ def mkKeyValuePair (k : Nat × Nat) (v : List $ Nat × Int) :
   (k, v)
 
 def elabSummand : TSyntax `summand → TermElabM Expr
-  | `(summand | ($b:num $c:num)) => do
-    mkAppM ``mkNatIntPair #[mkNatLit b.getNat, ← mkAppM ``Int.ofNat #[mkNatLit c.getNat]]
+  | `(summand | ($b:num $c:num)) =>
+    return mkAppN (.const ``mkNatIntPair [])
+      #[mkNatLit b.getNat, mkAppN (.const ``Int.ofNat []) #[mkNatLit c.getNat]]
   | `(summand | ($b:num -$c:num)) => do
-    let c ← match c.getNat with
-      | 0 => mkAppM ``Int.ofNat #[mkConst ``Nat.zero]
-      | n + 1 => mkAppM ``Int.negSucc #[mkNatLit n]
-    mkAppM ``mkNatIntPair #[mkNatLit b.getNat, c]
+    let c := match c.getNat with
+      | 0 => mkAppN (.const ``Int.ofNat []) #[.const ``Nat.zero []]
+      | n + 1 => mkAppN (.const ``Int.negSucc []) #[mkNatLit n]
+    return mkAppN (.const ``mkNatIntPair []) #[mkNatLit b.getNat, c]
   | _ => throwUnsupportedSyntax
 
 def elabProdType (n₁ n₂ : Name) : TermElabM Expr :=
-  mkAppM ``Prod #[mkConst n₁, mkConst n₂]
+  mkAppM ``Prod #[.const n₁ [], .const n₂ []]
 
 def elabPolynomial : TSyntax `polynomial → TermElabM Expr
   | `(polynomial | $w:num $ν:num # $ar:summand*) => do
-    let k ← mkAppM ``mkNatNatPair #[mkNatLit w.getNat, mkNatLit ν.getNat]
+    let k := mkAppN (.const ``mkNatNatPair []) #[mkNatLit w.getNat, mkNatLit ν.getNat]
     let ar ← ar.data.mapM elabSummand
     let natInt ← elabProdType ``Nat ``Int
-    mkAppM ``mkKeyValuePair #[k, ← mkListLit natInt ar]
+    return mkAppN (.const ``mkKeyValuePair []) #[k, ← mkListLit natInt ar]
   | _ => throwUnsupportedSyntax
 
 scoped elab "⟪" ps:polynomial,* "⟫" : term => do
