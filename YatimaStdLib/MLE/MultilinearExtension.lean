e142464738ac55acdec31309cc8f6ac31818fbfe
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-12-08 19:47:46 -0300

    caching finished. missing final cache (it's big)

--- YatimaStdLib/MLE/MultilinearExtension.lean
+++ YatimaStdLib/MLE/MultilinearExtension.lean
@@ -1,45 +1,9 @@
-import YatimaStdLib.MLE.Cache
-import YatimaStdLib.Array
-import YatimaStdLib.Ord
+import YatimaStdLib.MLE.MultilinearLagrangePolynomial
 
 namespace MLE
 
-/--
-Computes the multilinear Lagrange basis polynomial with interpolating set
-{0, 1}^ν.
--/
-def multilinearLagrangePolynomial (ν w : Nat) : MultilinearPolynomial $ Zmod n :=
-  List.range ν |>.foldl (init := .ofPairs [(0, 1)]) fun acc i =>
-    let wᵢ := w >>> i % 2
-    acc * .prune (.ofPairs [(1 <<< i, 2 * wᵢ - 1), (0, 1 - wᵢ)])
-
-open Lean (Json)
-
-def jsonCache (νMax : Nat) : Array Json :=
-  List.range νMax.succ |>.foldl (init := #[]) fun acc ν =>
-    List.range νMax.succ |>.foldl (init := acc) fun acc w =>
-      let pol := @multilinearLagrangePolynomial 0 ν w
-        |>.foldl (init := #[]) fun acc b c => acc.push $ .arr #[b, c]
-      acc.push $ .arr #[ν, w, .arr pol]
-
-def writeCache (νMax : Nat) : IO Unit := do
-  let json : Json := .arr $ ← (jsonCache νMax).shuffle (some 42)
-  IO.FS.writeFile ⟨"YatimaStdLib/MLE/cache.json"⟩ (json.pretty 200)
-
--- Uncomment this line to write the cache file (be careful with the parameter)
--- #eval writeCache 4
-
 /-- Computes the multilinear extension of a function `f : {0, 1}^ν → Zmod n` -/
 def multilinearExtension (f : Nat → Zmod n) (ν : Nat) : MultilinearPolynomial $ Zmod n :=
-  .prune $ List.range (1 <<< ν) |>.foldl (init := default) fun acc w =>
-    acc + .scale (multilinearLagrangePolynomial ν w) (f w)
-
-def multilinearLagrangePolynomialCache :
-    Std.RBMap (Nat × Nat) (MultilinearPolynomial $ Zmod n) compare :=
-  cachedMLPData.foldl (init := default) fun acc (k, v) =>
-    acc.insert k (.ofList v _)
-
-def multilinearExtension' (f : Nat → Zmod n) (ν : Nat) : MultilinearPolynomial $ Zmod n :=
   .prune $ List.range (1 <<< ν) |>.foldl (init := default) fun acc w =>
     let pol := match multilinearLagrangePolynomialCache.find? (ν, w) with
       | some pol => pol
@@ -54,7 +18,6 @@ open LSpec
 def f : Nat → Zmod 5 := fun _ => 0
 
 #lspec
-  test "" (multilinearExtension f 0 == multilinearExtension' f 0) $
   test "" (multilinearExtension f 0 == .ofSummandsL [])
 
 end Tests
---
+++ YatimaStdLib/MLE/MultilinearLagrangePolynomial.lean
@@ -0,0 +1,73 @@
+import YatimaStdLib.MultilinearPolynomial
+import YatimaStdLib.Zmod
+import YatimaStdLib.Array
+import YatimaStdLib.Ord
+import Lean.Data.Json
+
+namespace MLE
+
+/--
+Computes the multilinear Lagrange basis polynomial with interpolating set
+`{0, 1}^ν` for a given `w`.
+-/
+def multilinearLagrangePolynomial (ν w : Nat) : MultilinearPolynomial $ Zmod n :=
+  List.range ν |>.foldl (init := .ofPairs [(0, 1)]) fun acc i =>
+    let wᵢ := w >>> i % 2
+    acc * .prune (.ofPairs [(1 <<< i, 2 * wᵢ - 1), (0, 1 - wᵢ)])
+
+open Lean (Json)
+
+/--
+Creates an array of JSON data, each element encoding a polynomial. Adding `1` to
+the coefficients saves a few `-` characters because `-1`s turn into `0`s.
+-/
+def buildJsonCache (νMax : Nat) : Array Json :=
+  List.range νMax.succ |>.foldl (init := #[]) fun acc ν =>
+    List.range νMax.succ |>.foldl (init := acc) fun acc w =>
+      let pol := @multilinearLagrangePolynomial 3 ν w
+        |>.foldl (init := #[]) fun acc b c => acc.push $ .arr #[b, c + (1 : Int)]
+      acc.push $ .arr #[ν, w, .arr pol]
+
+/-- Writes cached polynomials as a JSON file -/
+def writeJsonCache (νMax : Nat) : IO Unit := do
+  let json := Json.arr $ buildJsonCache νMax
+  IO.FS.writeFile ⟨"YatimaStdLib/MLE/cache.json"⟩ json.compress
+
+-- Uncomment to write the cache file (be careful with the parameter):
+-- #eval writeJsonCache 4
+
+open Lean (Json)
+
+abbrev MLPData := (Nat × Nat) × List (Nat × Int)
+
+/-- Extracts data for one MLP from a `Json.arr` node -/
+def jsonToMLP : Json → Except String MLPData
+  | .arr #[ν, w, .arr summands] => do
+    let summands ← summands.foldlM (init := []) fun acc s => do
+      match s with
+      | .arr #[b, c] => pure $ (← b.getNat?, (← c.getInt?) - 1) :: acc
+      | x => throw s!"Unexpected JSON for MLP summand: {x.pretty}"
+    return ((← ν.getNat?, ← w.getNat?), summands)
+  | x => throw s!"Unexpected JSON for MLP: {x.pretty}"
+
+/-- Extracts data for many MLPs from a `Json.arr` node -/
+def jsonToArrayMLP : Json → Except String (Array MLPData)
+  | .arr MLPs => MLPs.mapM jsonToMLP
+  | x => throw s!"Unexpected JSON for MLP array: {x.pretty}"
+
+/-- Reads cached polynomials from a JSON file -/
+def readCachedMLPData : IO $ Array MLPData := do
+  match Json.parse (← IO.FS.readFile ⟨"YatimaStdLib/MLE/cache.json"⟩) with
+  | .error e => panic! e
+  | .ok cachedJson => match jsonToArrayMLP cachedJson with
+    | .error e => panic! e
+    | .ok cache => cache.shuffle (some 42) -- shuffling will mitigate unbalanced trees
+
+initialize cachedMLPData : Array MLPData ← readCachedMLPData
+
+/-- The actual cached polynomials -/
+def multilinearLagrangePolynomialCache :
+    Std.RBMap (Nat × Nat) (MultilinearPolynomial $ Zmod n) compare :=
+  cachedMLPData.foldl (fun acc (k, v) => acc.insert k (.ofList v _)) default
+
+end MLE
