5be3ba6d60bcfa89feb5eba65a18e13f694ba64d
mpenciak <matej.penciak@gmail.com>
2022-12-12 16:45:36 -0500

    use imported functions

--- YatimaStdLib/MLE/MultiLinearLagrangeTest.lean
+++ YatimaStdLib/MLE/MultiLinearLagrangeTest.lean
@@ -1,23 +1,8 @@
 import LSpec
 import YatimaStdLib.MLE.MultilinearLagrangeData
+import YatimaStdLib.Bit
 
 open MLE MultilinearPolynomial
-
-private def Array.padZero [OfNat α (nat_lit 0)] (ar : Array α) (n : Nat) : Array α :=
-  let diff := n - ar.size
-  ar ++ (.mkArray diff 0)
-
-/-- Generates the array of binary expansions between `0` and `2^n` -/
-private def getBins (n : Nat) : Array (Array Nat) := Id.run do
-  let mut answer := #[(.mkArray n 0)]
-  for x in [1:2^n] do
-    let xBits := x |> (Nat.toDigits 2 ·) 
-                   |>.map (fun c => c.toNat - 48)
-                   |>.reverse
-                   |>.toArray
-                   |>.padZero n
-    answer := answer.push xBits
-  return answer
 
 /-- 
 Tests whether the multilinear Lagrange polynomials satisfy their defining property: 
@@ -25,13 +10,12 @@ Tests whether the multilinear Lagrange polynomials satisfy their defining proper
 -/
 def buildTests (size : Nat) : Bool := Id.run do
   let mut results := #[]
-  let cases := getBins size
+  let cases := getBits size
   for t in [:2^size] do
     for t' in [:2^size] do
       let res := if t == t' then 1 else 0
       let b := eval (@multilinearLagrangePolynomial 13 size t) cases[t']! == res
       results := results.push b
-  dbg_trace results
   results.all (· == true)
 
 open LSpec
