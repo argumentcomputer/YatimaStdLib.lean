44894c55e2c6bf150631c1848d16dd9d3ea6a38f
DanielRrr <daniel.rogozin@serokell.io>
2022-12-12 16:53:13 +0400

    complete all required algebraic operations on sparse matrices

--- YatimaStdLib/SparseMatrix.lean
+++ YatimaStdLib/SparseMatrix.lean
@@ -1,5 +1,6 @@
 import YatimaStdLib.Array
 import YatimaStdLib.Ring
+import YatimaStdLib.RBMap
 import Std.Data.RBMap
 
 def ordCmp (a : Nat × Nat) (b : Nat × Nat) : Ordering :=
@@ -8,8 +9,8 @@ def ordCmp (a : Nat × Nat) (b : Nat × Nat) : Ordering :=
     | _    => if a == b then .eq else .gt
 
 abbrev SparseMatrix (R : Type) := Std.RBMap (Nat × Nat) R ordCmp
-abbrev Row (R : Type) := Std.RBMap Nat R instOrdNat.compare
-abbrev Col (R : Type) := Std.RBMap Nat R instOrdNat.compare
+abbrev Row (R : Type) := Array R
+abbrev Col (R : Type) := Array R
 
 variable {R : Type} [Ring R] [Coe Nat R]
 
@@ -22,13 +23,23 @@ def SparseMatrix.cols (m : SparseMatrix R) : Nat :=
   1 + (Array.foldr max 0 $ Array.map (fun p => p.2) $ keysArray m)
 
 def SparseMatrix.dim (m : SparseMatrix R) : Nat × Nat :=
-  (rows m, cols m) 
+  (rows m, cols m)
+
+def SparseMatrix.findEntry (m : SparseMatrix R) (p : Nat × Nat) : Nat × Nat × R :=
+  let ((row, col), val) := Option.getD (Std.RBMap.findEntry? m p) ((0,0),0)
+  (row, col, val)
+
+def SparseMatrix.findElem (m : SparseMatrix R) (p : Nat × Nat) : R :=
+  let (_, val) := Option.getD (Std.RBMap.findEntry? m p) ((0,0),0)
+  val
 
 def SparseMatrix.row (m : SparseMatrix R) (i : Nat) : Row R :=
-  Std.RBMap.filter m (fun j _ => i == j.1)
+  Std.RBMap.valuesArray $
+    Std.RBMap.mapKeys (Std.RBMap.filter m (fun j _ => i == j.1)) (fun (a,_) => a)
 
 def SparseMatrix.col (m : SparseMatrix R) (i : Nat) : Col R :=
-  Std.RBMap.filter m (fun j _ => i == j.2)
+  Std.RBMap.valuesArray $
+    Std.RBMap.mapKeys (Std.RBMap.filter m (fun j _ => i == j.2)) (fun (_,b) => b)
 
 instance : Functor SparseMatrix where
   map f m := Std.RBMap.mapVal (fun _ x => f x) m
@@ -57,30 +68,47 @@ def mat2 : SparseMatrix Int := SparseMatrix.ofList [((0,0),3), ((0,1),9), ((1,0)
 #eval SparseMatrix.addition mat1 mat2
 -/
 
+def SparseMatrix.vecProduct (m : SparseMatrix R) (v : Col R) : Col R := Id.run do
+  let (rows,cols) := (m.rows, m.cols)
+  let vals : Array (Nat × R) := Id.run do
+    let mut acc := Array.empty
+    for i in [0:rows] do
+      for k in [0:cols] do
+        let (row, col, val) := SparseMatrix.findEntry m (i,k)
+        acc := Array.push acc (row, val * Array.getD v col 0)
+    pure acc
+  Array.foldr 
+    (fun (r,v) mz => Array.setD mz r (v + Array.getD mz r 0)) 
+    #[0, rows] 
+    vals
+
 def SparseMatrix.multiplication
-  (m₁ : SparseMatrix R) (m₂ : SparseMatrix R) : SparseMatrix R :=
+  (m₁ : SparseMatrix R) (m₂ : SparseMatrix R) : SparseMatrix R := Id.run do
   let cols₂ := m₂.cols
   let rows₁ := m₁.rows
   if rows₁ == cols₂ then
-    let cij i j := sorry
-  else panic "wrong dim"
+    let cij i j : R := Array.foldr (. + .) 0 (Array.map (fun (a, b) => a * b) (Array.zip (m₁.row i) (m₂.col j)))
+    let mut acc : SparseMatrix R := Std.RBMap.empty
+    for i in [0:rows₁] do
+      for j in [0:cols₂] do
+      acc := Std.RBMap.insert acc (i,j) (cij i j)
+    pure acc
+  else
+    panic! "wrong dim"
 
 def SparseMatrix.hadamard
-  (m₁ : SparseMatrix R) (m₂ : SparseMatrix R) : SparseMatrix R := sorry
-
-def SparseMatrix.dotProduct
-  (m₁ : SparseMatrix R) (m₂ : SparseMatrix R) : R := sorry
-
-def SparseMatrix.vecProduct (m : SparseMatrix R) (numRows : USize) (z : Array R) : Array R :=
-  let vals := Array.map
-      (fun i =>
-        let (row, col, val) := Array.getD m i (0, 0, 0)
-        (row, val * Array.getD z col 0))
-      (Array.iota m.size)
-  Array.foldr
-    (fun (r,v) mz => Array.setD mz r (v + Array.getD mz r 0))
-    #[0, numRows.val]
-    vals
+  (m₁ : SparseMatrix R) (m₂ : SparseMatrix R) : SparseMatrix R := Id.run do
+  if m₁.dim == m₂.dim then
+    let mut acc : SparseMatrix R := Std.RBMap.empty
+    let (rows, cols) := m₁.dim
+    for i in [0:rows] do
+      for j in [0:cols] do
+      acc :=
+        Std.RBMap.insert acc (i,j)
+          (SparseMatrix.findElem m₁ (i,j) * SparseMatrix.findElem m₂ (i,j))
+    pure acc
+  else
+    panic! "wrong dim"
 
 def SparseMatrix.scale
   (r : R) (m : SparseMatrix R) : SparseMatrix R := (fun x => x * r) <$> m
