d15ca7d1af82f55a7d2bb7e15c93e6b0dc16d332
DanielRrr <daniel.rogozin@serokell.io>
2022-12-21 19:38:19 +0400

    add Coe Nat R for Rings

--- YatimaStdLib/Ring.lean
+++ YatimaStdLib/Ring.lean
@@ -2,5 +2,13 @@ class Ring (R : Type) extends Add R, Mul R, Sub R, OfNat R (nat_lit 0), OfNat R 
 
 instance [Add R] [Mul R] [Sub R] [OfNat R 0] [OfNat R 1] [HPow R Nat R] : Ring R := {}
 
+def natToRing [Ring R] (n : Nat) : R :=
+  match n with
+    | 0 => 0
+    | k+1 => natToRing k + 1
+
+instance [Ring R] : Coe Nat R where
+  coe := natToRing
+
 instance [Ring R] : Inhabited R where
   default := 0
