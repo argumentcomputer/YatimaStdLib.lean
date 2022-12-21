fdbb93deec4f6c56a646802e380c569abb038e2f
Daniel Rogozin <relationalgebra@gmail.com>
2022-12-21 18:40:06 +0400

    Add the Sub class for Ring (#54)
    Actually, every ring has subtraction as well, so this PR introduces it

--- YatimaStdLib/Ring.lean
+++ YatimaStdLib/Ring.lean
@@ -1,6 +1,6 @@
-class Ring (R : Type) extends Add R, Mul R, OfNat R (nat_lit 0), HPow R Nat R
+class Ring (R : Type) extends Add R, Mul R, Sub R, OfNat R (nat_lit 0), HPow R Nat R
 
-instance [Add R] [Mul R] [OfNat R 0] [HPow R Nat R] : Ring R := {}
+instance [Add R] [Mul R] [Sub R] [OfNat R 0] [HPow R Nat R] : Ring R := {}
 
 instance [Ring R] : Inhabited R where
   default := 0
