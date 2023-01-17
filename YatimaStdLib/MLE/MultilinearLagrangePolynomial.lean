869f0d97e7743579ab36531fd89ab3363a8fb3b9
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-17 11:45:55 -0300

    fix MLE sources

--- YatimaStdLib/MLE/MultilinearLagrangePolynomial.lean
+++ YatimaStdLib/MLE/MultilinearLagrangePolynomial.lean
@@ -3,10 +3,13 @@ import YatimaStdLib.Ord
 
 namespace MLE
 
+scoped instance : Coe (List (Nat × Int)) (List (Nat × Zmod n)) where
+  coe x := x.map fun (n, i) => (n, i)
+
 /-- Reads cached polynomials from a JSON file -/
 def polynomialsFromCache :
     IO $ Std.RBMap (Nat × Nat) (MultilinearPolynomial $ Zmod n) compare :=
-  return cachedMLPData.foldl (fun acc (k, v) => acc.insert k (.ofList v _)) default
+  return cachedMLPData.foldl (fun acc (k, v) => acc.insert k (.ofPairs v)) default
 
 def pallasFSize := 0x40000000000000000000000000000000224698fc094cf91b992d30ed00000001
 def vestaFSize  := 0x40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001
