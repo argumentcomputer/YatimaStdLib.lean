14346aa32a8274344e9323e5df9d7a8673421060
DanielRrr <relationalgebra@gmail.com>
2023-01-17 14:47:46 +0000

    introduce the Field class

--- YatimaStdLib/Ring.lean
+++ YatimaStdLib/Ring.lean
@@ -1,4 +1,4 @@
-class Ring (R : Type) extends Add R, Mul R, Sub R, OfNat R (nat_lit 0), OfNat R (nat_lit 1), HPow R Nat R
+class Ring (R : Type) extends Add R, Mul R, Sub R, OfNat R (nat_lit 0), OfNat R (nat_lit 1), HPow R Nat R, BEq R
 
 def natToRing [Ring R] (n : Nat) : R :=
   match n with
@@ -8,5 +8,22 @@ def natToRing [Ring R] (n : Nat) : R :=
 instance [Ring R] : Coe Nat R where
   coe := natToRing
 
+instance [Ring R] : OfNat R n where
+  ofNat := Coe.coe n
+
 instance [Ring R] : Inhabited R where
   default := 0
+
+class Field (K : Type) extends Ring K where
+  inv : K → K
+  sqrtRatio : K → K → Bool × K
+
+/-- Doubles this element -/
+def double [Field K] (k : K) : K := 2 * k
+
+/-- Returns the square root of the field element, if it is a quadratic residue -/
+def sqrt [Field K] (k : K) : Option K :=
+  let (isSquare, res) := Field.sqrtRatio k 1
+  match isSquare with
+  | false => none
+  | true => some res
