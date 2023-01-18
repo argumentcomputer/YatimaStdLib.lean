a75b8c629a46c372c43f69876efd37988518b469
mpenciak <matej.penciak@gmail.com>
2023-01-17 19:20:41 -0500

    handwritten tests

--- Tests/AddChain.lean
+++ Tests/AddChain.lean
@@ -7,5 +7,18 @@ open Lean LSpec
 TODO: Getting a stack overflow when running this.
 (also getting a stack overflow without the `fastExp` side of the equation)
 -/
-def main := lspecIO $ 
-  check "fastExp works" $ (∀ n m : Nat, fastExp n m = n ^ m)
+def main : IO UInt32 := do
+  let bytes ← IO.getRandomBytes 8
+  IO.setRandSeed bytes.toUInt64BE!.toNat
+
+  IO.println "Starting test"
+
+  for _ in [:1000] do
+    let n ← IO.rand 0 1000000
+    let m ← IO.rand 0 1000000
+    unless fastExp n m == n ^ m do
+      IO.println s!"Found an error: fastExp {n} {m} != {n} ^ {m}"
+      return 1
+
+  IO.println "Tests passed!"
+  return 0
