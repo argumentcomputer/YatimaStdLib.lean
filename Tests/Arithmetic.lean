a824729b5b611b0e18cfd80b177a6370f8930fc3
Matej Penciak <matej.penciak@gmail.com>
2023-02-05 22:37:38 -0500

    test was too big lol

--- Tests/Arithmetic.lean
+++ Tests/Arithmetic.lean
@@ -4,7 +4,7 @@ import LSpec
 open LSpec
 
 def main := lspecIO $
-  test "works on big inputs" (dLog 37
-         18094411505653743630246040579337206337386467465616016462821305448438411906485
-         0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001 == 
-         some 0x72eda753299d7d473339d80809a1d80553bda402fffe5bfeffffffff00000001)
+  test "works on big inputs" (dLog 39
+         519726712005
+         843929400311 == 
+         some 83483865923)
