b54637afa8b7744d74399036d80ef703e2136cc0
Matej Penciak <matej.penciak@gmail.com>
2023-02-08 21:28:49 -0500

    delete problematic instances

--- YatimaStdLib/Ring.lean
+++ YatimaStdLib/Ring.lean
@@ -6,17 +6,6 @@ namespace Ring
 instance {R : Type _} [Add R] [Mul R] [Sub R] [OfNat R (nat_lit 0)] [OfNat R (nat_lit 1)] 
   [HPow R Nat R] [BEq R] : Ring R where
 
-def fromNat [Ring R] (n : Nat) : R :=
-  match n with
-    | 0 => 0
-    | k + 1 => fromNat k + 1
-
-instance [Ring R] : Coe Nat R where
-  coe := fromNat
-
-instance [Ring R] : OfNat R n where
-  ofNat := Coe.coe n
-
 instance [Ring R] : Inhabited R where
   default := 0
 
