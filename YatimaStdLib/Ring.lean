db19e7fda300944bb789cb77a511d442cb6f6749
Matej Penciak <matej.penciak@gmail.com>
2023-02-15 18:59:47 -0500

    last fix?

--- YatimaStdLib/Ring.lean
+++ YatimaStdLib/Ring.lean
@@ -1,10 +1,19 @@
-class Ring (R : Type) extends Add R, Mul R, Sub R, OfNat R (nat_lit 0), OfNat R (nat_lit 1), 
-  HPow R Nat R, BEq R
+class Ring (R : Type) extends Add R, Mul R, Sub R, HPow R Nat R, BEq R where
+  zero : R
+  one : R
 
 namespace Ring
 
 instance {R : Type _} [Add R] [Mul R] [Sub R] [OfNat R (nat_lit 0)] [OfNat R (nat_lit 1)] 
   [HPow R Nat R] [BEq R] : Ring R where
+  zero := 0
+  one := 1
+
+instance [Ring R] : OfNat R (nat_lit 0) where
+  ofNat := zero
+
+instance [Ring R] : OfNat R (nat_lit 1) where
+  ofNat := one
 
 instance [Ring R] : Inhabited R where
   default := 0
